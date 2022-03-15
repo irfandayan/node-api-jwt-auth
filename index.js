448e3a187b92d90425e3e3e26f03ca107f0801f1
irfan dayan <engr.irfandayan@gmail.com>
2022-03-15 11:07:45 +0500

    Connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
 
 const app = express();
+
+// Connect to DB
+mongoose.connect(
+  "mongodb+srv://irfandayan:dayan%4045%4045@cluster0.psjfd.mongodb.net/cluster0?retryWrites=true&w=majority",
+  () => console.log("connected to db!")
+);
+
+// Router middleware
+app.use("/api/user", authRoute);
 
 app.listen(3000, () => console.log("Server Up and running "));
