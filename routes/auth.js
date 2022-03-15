249654a440281b39509da48afb89c2e40c596755
irfan dayan <engr.irfandayan@gmail.com>
2022-03-15 12:53:57 +0500

    Add validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,12 @@
 import express from "express";
 import User2 from "../model/User.js";
-import Joi from "@hapi/joi";
 
 const router = express.Router();
 
-// Validation
-const validationSchema = Joi.object({
-  name: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
-
 router.post("/register", async (req, res) => {
-  // Let's validate the date before we create a user
-
-  const { error } = validationSchema.validate(req.body);
-  //   res.send(error.details[0].message);
-  if (error) return res.status(400).send(error.details[0].message);
+  //   // Let's validate the date before we create a user
+  //   const { error } = validationSchema.validate(req.body);
+  //   if (error) return res.status(400).send(error.details[0].message);
 
   const user2 = new User2({
     name: req.body.name,
