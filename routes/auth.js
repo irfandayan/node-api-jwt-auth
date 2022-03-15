f89376794ea2b5405830072d4e96d7e3cb29e3ac
irfan dayan <engr.irfandayan@gmail.com>
2022-03-15 14:34:14 +0500

    Add login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,61 @@
+import bcrypt from "bcryptjs";
 import express from "express";
-import User2 from "../model/User.js";
+
+import User from "../model/User.js";
+import { registerValidation, loginValidation } from "../validation.js";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
   //   // Let's validate the date before we create a user
   //   const { error } = validationSchema.validate(req.body);
-  //   if (error) return res.status(400).send(error.details[0].message);
 
-  const user2 = new User2({
+  // Validation
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if user already exist
+  const emailExist = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  // Hash/Encrypt passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create a new user
+  const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
-    const savedUser = await user2.save();
-    res.send(savedUser);
+    const savedUser = await user.save();
+    res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+router.post("/login", async (req, res) => {
+  // Validation
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Check if email exist
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (!user) return res.status(400).send("Email is not found");
+  // Password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  res.send("Loged in");
 });
 
 export default router;
