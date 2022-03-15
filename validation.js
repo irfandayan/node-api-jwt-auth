19a3c484ab5009ab4ad3306e4b79da94072ae8ab
irfan dayan <engr.irfandayan@gmail.com>
2022-03-15 14:35:04 +0500

    add validation.js

---
+++ validation.js
@@ -0,0 +1,22 @@
+import Joi from "@hapi/joi";
+
+// Register validation
+export const registerValidation = (data) => {
+  const validationSchema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return validationSchema.validate(data);
+};
+
+// Login validation
+export const loginValidation = (data) => {
+  const validationSchema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+
+  return validationSchema.validate(data);
+};
