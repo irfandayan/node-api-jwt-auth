c6c6af58aecc4878d785620ecba87f3675a05f44
irfan <engr.irfandayan@gmail.com>
2022-08-25 12:59:23 +0500

    index rename to server

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getPosts } from "../controllers/posts.js";
-import verifyToken from "../controllers/verifyToken.js";
+import verifyToken from "../middlewares/verifyToken.js";
 const router = express.Router();
 
 router.get("/", verifyToken, getPosts);
